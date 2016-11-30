38dcc93913dd7eda009de178052133ce461b99d2
Phong Huynh <xphong@gmail.com>
2016-11-30 13:34:07 -0500

    Use bodyParser, env ports

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -14,22 +15,24 @@ if (app.get('env') === 'development') {
 
 compiler = webpack(config);
 
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
-
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', (err) => {
+app.listen(app.get('port'), 'localhost', (err) => {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log('Listening on port ' + app.get('port'));
 });
