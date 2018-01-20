1d1c301cfa22234ef710d9ad1401ac2c21aa86ba
Phong Huynh <xphong@gmail.com>
2018-01-20 09:07:06 -0500

    Add render test to App

--- client/__tests__/App-test.js
+++ client/__tests__/App-test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from '../containers/App';
+import Nav from '../components/ui/Nav';
 
 describe('App', () => {
   it('should render App', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+  });
+
+  it('should render Nav', () => {
+      const app = shallow(<App/>);
+      const nav = <Nav />;
+
+      expect(app.contains(nav)).toEqual(true);
   });
 });
