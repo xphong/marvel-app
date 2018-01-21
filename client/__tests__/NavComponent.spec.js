3b5236f1951f81bb05222a1503ad66b6babd8775
Phong Huynh <xphong@gmail.com>
2018-01-21 10:55:12 -0500

    Add test for Nav component

--- client/__tests__/NavComponent.spec.js
+++ client/__tests__/NavComponent.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from '../containers/App';
+import Nav from '../components/ui/Nav';
 
-describe('App Container', () => {
-  it('should render App', () => {
+describe('Nav Component', () => {
+  it('should render Nav', () => {
     const wrapper = shallow(
-      <App />
+      <Nav />
     );
 
     expect(wrapper).toMatchSnapshot();
