b8c53001660f13df07b62dc7351497e58724d388
Phong Huynh <xphong@gmail.com>
2018-01-21 18:10:00 -0500

    Add separate spec for character actions snapshot

--- client/__tests__/CharactersActions.spec.js
+++ client/__tests__/CharactersActions.spec.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import moxios from 'moxios';
 import thunk from 'redux-thunk';
 
+import * as types from '../constants/ActionTypes';
 import * as CharactersActions from '../actions/CharactersActions';
 import * as mocks from './mocks/charactersMock';
 
@@ -18,6 +19,33 @@ describe('Characters Actions: fetchCharactersByName', () => {
   });
 
   it('should get characters by name', () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: mocks.charactersAPIMock
+      });
+    });
+
+    const expectedResult = [
+      { type: types.REQUEST_CHARACTERS_BY_NAME },
+      {
+        type: types.RECEIVE_CHARACTERS,
+        payload: {
+          data: mocks.charactersDataMock
+        }
+      }
+    ];
+
+    const store = mockStore({});
+
+    return store.dispatch(CharactersActions.fetchCharactersByName('Venom'))
+    .then(() => {
+      expect(store.getActions()).toEqual(expectedResult);
+    });
+  });
+
+  it('should get characters by name snapshot', () => {
     moxios.wait(() => {
       const request = moxios.requests.mostRecent();
       request.respondWith({
