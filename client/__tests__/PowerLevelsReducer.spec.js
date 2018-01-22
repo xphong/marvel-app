6c7d1d0c9855d562f1b60fd7a7e1888f98b384b5
Phong Huynh <xphong@gmail.com>
2018-01-21 19:15:23 -0500

    Add tests for power levels reducer

---
+++ client/__tests__/PowerLevelsReducer.spec.js
@@ -0,0 +1,48 @@
+import * as types from '../constants/ActionTypes';
+import powerlevelsReducer from '../reducers/powerlevels';
+
+describe('Power Levels Reducer', () => {
+  it('should return initial state', () => {
+    const reducer = powerlevelsReducer(undefined, {});
+
+    expect(reducer).toMatchSnapshot();
+  });
+
+  it('should handle RECEIVE_POWERLEVELS_ERROR', () => {
+    const action = {
+      type: types.RECEIVE_POWERLEVELS_ERROR,
+      payload: {
+        data: []
+      }
+    };
+
+    const reducer = powerlevelsReducer(undefined, action);
+
+    expect(reducer).toMatchSnapshot();
+  });
+
+  it('should handle RECEIVE_POWERLEVELS', () => {
+    const action = {
+      type: types.RECEIVE_POWERLEVELS,
+      payload: {
+        data: [{
+          name: 'Spider-man'
+        }]
+      }
+    };
+
+    const reducer = powerlevelsReducer(undefined, action);
+
+    expect(reducer).toMatchSnapshot();
+  });
+
+  it('should handle REQUEST_POWERLEVELS', () => {
+    const action = {
+      type: types.REQUEST_POWERLEVELS
+    };
+
+    const reducer = powerlevelsReducer(undefined, action);
+
+    expect(reducer).toMatchSnapshot();
+  });
+});
