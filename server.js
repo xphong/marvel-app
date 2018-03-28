fc8a9441452f9d532b7e08a945b08a3fc3e0ba8f
Phong Huynh <xphong@gmail.com>
2018-03-27 21:34:54 -0400

    Update express server to use index.html

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
-const Router = require('react-router');
 
-const routes = require('./client/routes');
 const config = require('./server/config');
 
 const app = express();
@@ -43,18 +39,8 @@ app.use(webpackHotMiddleware(webpackCompiler));
 app.use('/api/v1/powerlevels', require('./server/powerlevels/powerlevelsRoutes'));
 app.use('/api/v1/characters', require('./server/characters/charactersRoutes'));
 
-app.use(function(req, res) {
-  Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
-    if (err) {
-      res.status(500).send(err.message)
-    } else if (redirectLocation) {
-      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
-    } else if (renderProps) {
-      res.status(200).send(renderFullPage());
-    } else {
-      res.status(404).send('Page Not Found')
-    }
-  });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(app.get('port'), (err) => {
@@ -65,25 +51,5 @@ app.listen(app.get('port'), (err) => {
 
   console.log('Listening on port ' + app.get('port'));
 });
-
-function renderFullPage() {
-  return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Marvel App - Phong Huynh</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
-      </head>
-
-      <body>
-        <div id="root">
-          Loading...
-        </div>
-      </body>
-
-      <script src="/static/bundle.js"></script>
-    </html>
-  `;
-}
 
 module.exports = app;
