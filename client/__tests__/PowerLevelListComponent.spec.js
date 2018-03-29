06e646f75682457bd6706f9a5f2fcc93375a71e1
Phong Huynh <xphong@gmail.com>
2018-03-29 17:21:47 -0400

    Add tests for power level list and row components

---
+++ client/__tests__/PowerLevelListComponent.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import PowerLevelList from "../components/powerlevels/PowerLevelList";
+
+describe('PowerLevelList Component', () => {
+  it('should render PowerLevelList', () => {
+    const props = {
+      actions: {},
+      powerLevels: {
+        data: [{
+          Name: 'Storm',
+          AveragePowerLevel: 9000
+        }]
+      }
+    };
+
+    const wrapper = shallow(<PowerLevelList {...props} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
---
+++ client/__tests__/PowerLevelRowComponent.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import PowerLevelRow from "../components/powerlevels/PowerLevelRow";
+
+describe('PowerLevelRow Component', () => {
+  it('should render PowerLevelRow', () => {
+    const props = {
+      characterData: {
+        Name: 'Storm',
+        AveragePowerLevel: 9000
+      },
+      index: 1
+    };
+
+    const wrapper = shallow(<PowerLevelRow {...props} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
